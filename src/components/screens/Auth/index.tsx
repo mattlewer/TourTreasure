0a207ee7c5c77f531d1d2a8fadd41593676da511
Matt <mlewer@hotmail.co.uk>
2023-08-15 23:01:53 +0100

    Swap background image: Login/Signup

--- src/components/screens/Auth/index.tsx
+++ src/components/screens/Auth/index.tsx
@@ -1,6 +1,6 @@
 import {ImageBackground, StyleSheet} from 'react-native';
 import React from 'react';
-import SplashScreen from '../../../assets/splash_screen.png';
+import SplashScreenNoLogo from '../../../assets/splash_screen_no_logo.png';
 import * as color from '../../../constants/color';
 import ScreenContainer from '../../modules/ScreenContainer';
 import CreateAccount from '../CreateAccount';
@@ -12,7 +12,7 @@ const Auth = () => {
   const viewModel = useAuthViewModel();
   return (
     <ScreenContainer scrollable stripPadding>
-      <ImageBackground style={style.container} source={SplashScreen}>
+      <ImageBackground style={style.container} source={SplashScreenNoLogo}>
         {viewModel.stage === LoginState.CREATE_ACCOUNT ? (
           <CreateAccount
             onCreateAccount={viewModel.onCreateAccount}
