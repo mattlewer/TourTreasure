07e4f0082f1c5563c5e8255307fd1bfda3d0fe61
Matt <mlewer@hotmail.co.uk>
2023-08-28 18:01:53 +0100

    Reset password from login

--- src/components/screens/Auth/index.tsx
+++ src/components/screens/Auth/index.tsx
@@ -3,26 +3,44 @@ import React from 'react';
 import SplashScreenNoLogo from '../../../assets/splash_screen_no_logo.png';
 import * as color from '../../../constants/color';
 import ScreenContainer from '../../modules/ScreenContainer';
-import CreateAccount from '../../modules/CreateAccount';
+import CreateAccount from '../../modules/AuthSections/CreateAccount';
 import useAuthViewModel from '../../../services/viewModels/screens/useAuthViewModel';
-import SignIn from '../../modules/SignIn';
+import SignIn from '../../modules/AuthSections/SignIn';
 import {LoginState} from '../../../enums/loginState';
 import LoadingOverlay from '../../modules/LoadingOverlay';
+import ResetPassword from '../../modules/AuthSections/ResetPassword';
 
 const Auth = () => {
   const viewModel = useAuthViewModel();
-  return (
-    <ScreenContainer scrollable stripPadding>
-      {viewModel.isLoading && <LoadingOverlay />}
-      <ImageBackground style={style.container} source={SplashScreenNoLogo}>
-        {viewModel.stage === LoginState.CREATE_ACCOUNT ? (
+
+  const renderScene = () => {
+    switch (viewModel.stage) {
+      case LoginState.CREATE_ACCOUNT:
+        return (
           <CreateAccount
             onCreateAccount={viewModel.onCreateAccount}
             setStage={viewModel.setStage}
           />
-        ) : (
+        );
+      case LoginState.SIGN_IN:
+        return (
           <SignIn onSignIn={viewModel.onSignIn} setStage={viewModel.setStage} />
-        )}
+        );
+      case LoginState.RESET_PASSWORD:
+        return (
+          <ResetPassword
+            onSendResetMail={viewModel.onSendResetEmail}
+            setStage={viewModel.setStage}
+          />
+        );
+    }
+  };
+
+  return (
+    <ScreenContainer scrollable stripPadding>
+      {viewModel.isLoading && <LoadingOverlay />}
+      <ImageBackground style={style.container} source={SplashScreenNoLogo}>
+        {renderScene()}
       </ImageBackground>
     </ScreenContainer>
   );
--- src/enums/loginState.ts
+++ src/enums/loginState.ts
@@ -1,4 +1,5 @@
 export enum LoginState {
   SIGN_IN = 1,
   CREATE_ACCOUNT = 2,
+  RESET_PASSWORD = 3,
 }
